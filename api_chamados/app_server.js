7e854c127eeb387c52246b3d1691147d165a3cfa
JaoBolao1234 <95043271+JaoBolao1234@users.noreply.github.com>
2022-08-12 14:51:10 -0300

    instalar os pacotes

--- api_chamados/app_server.js
+++ api_chamados/app_server.js
@@ -6,6 +6,13 @@ const port = 3000
 app.get('/abrir-chamado',(req,res)=>{
     res.send('Hello World')
 })
+app.post('/abrir-chamado/post',(req,res) =>{
+    res.send('hello post chamado')
+})
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`Exemplo app listening on port ${port}`)
